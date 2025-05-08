57c4f428b755cfa65576def858cbab686c78b4ca
JazielHeredia <148661084+Jazielillo@users.noreply.github.com>
2025-05-07 20:13:47 -0700

    Opción para Eliminar Paciente Implementado y Funcionando

--- src/componentes/PatientDetails.tsx
+++ src/componentes/PatientDetails.tsx
@@ -1,13 +1,44 @@
 import type { Patient } from "../types"
+import PatientDetailItem from "./PatientDetailItem"
+import { usePatientStore } from '../store/store'
 
 type PatientDetailsProps = {
     patient: Patient
 }
 
 export default function PatientDetails({ patient }: PatientDetailsProps) {
+
+    const deletePatient = usePatientStore(state => state.deletePatient)
+
     return (
-        <div>
-            {patient.name}
+        <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+
+            <PatientDetailItem label="ID" data={patient.id} />
+
+            <PatientDetailItem label="Nombre" data={patient.name} />
+
+            <PatientDetailItem label="Propietario" data={patient.caretaker} />
+
+            <PatientDetailItem label="Email" data={patient.email} />
+
+            <PatientDetailItem label="Sintomas" data={patient.symptoms} />
+
+            <PatientDetailItem label="Fecha" data={patient.date.toString()} />
+
+            <div className="flex justify-around mt-10">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white uppercase rounded-lg"
+                > Editar
+                </button>
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white uppercase rounded-lg"
+                    onClick={() => deletePatient(patient.id)}
+                > Eliminar
+                </button>
+            </div>
+
         </div>
     )
 }
